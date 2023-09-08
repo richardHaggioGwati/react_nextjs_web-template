5c38c9d8259b900d3199a2ec13a073989c7b8d0f
richardHaggioGwati <haggiog0844@gmail.com>
2023-09-08 18:39:20 +0200

    feat(server): amend

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("I will be shown on the Browser");
+    res.json("All the way from the back.....");
 });
 
 app.listen(5000);
